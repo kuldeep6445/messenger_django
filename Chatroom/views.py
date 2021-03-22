a02761c8af394fec0fc01ecb6dd50d49a2e46174
kuldeep6445 <kuldeepbhogal111@gmail.com>
2021-03-22 10:17:04 +0530

    added

--- Chatroom/views.py
+++ Chatroom/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate 
+from django.contrib.auth.models import User
 
 def check_login(request):
     if request.user.is_authenticated:
@@ -9,7 +10,13 @@ def check_login(request):
         return redirect('account/login/')
 
 def chatview(request):
-    context = {'logged_in_name':request.user.username}
+    t = request.POST.get('xyz','')
+    l = []
+    for x in User.objects.filter(username__contains='t'):
+        l.append(x.username)
+        print(x.username)
+    print(l)
+    context = {'logged_in_name':request.user.username,'items':l}
     return render(request,'Chatroom/index.html',context)
 
 def signup_view(request):
